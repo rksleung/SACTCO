d9f5554e520c8951d23b3982efe39837ae51c437
rksleung <rleung@netinfo.ubc.ca>
2020-12-31 21:17:58 -0800

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/dataworksbi/sacTCO/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.dataworksbi.sacTCO.controller.MainView", {});
+});
