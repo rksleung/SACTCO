efdc926f622a56bf9b4d1c9e1ff7fe588a981b9e
rksleung <rleung@netinfo.ubc.ca>
2021-01-01 18:42:26 -0800

    20210101 - SACTCO

--- uimodule/webapp/controller/MainView.controller.js
+++ uimodule/webapp/controller/MainView.controller.js
@@ -1,7 +1,16 @@
 sap.ui.define([
-  "com/dataworksbi/sacTCO/controller/BaseController"
-], function(Controller) {
+  "com/dataworksbi/sacTCO/controller/BaseController",
+  "com/dataworksbi/sacTCO/controller/ModelUtils",
+], function(Controller, ModelUtils) {
   "use strict";
 
-  return Controller.extend("com.dataworksbi.sacTCO.controller.MainView", {});
+  return Controller.extend("com.dataworksbi.sacTCO.controller.MainView", {
+    modelUtils: new ModelUtils(),
+
+    onInit: function () {
+      var tcoChartModel = this.modelUtils.getTCOChartData();
+      tcoChartModel.loadData("model/financials.json", "", false);
+			this.getView().setModel(tcoChartModel, "TCOData");      
+    }
+  });
 });
